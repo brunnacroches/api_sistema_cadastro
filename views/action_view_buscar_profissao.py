2b62e91f89ef00a38470f22edf3fcca6cbbbdb91
Brunna <brunnacroches@gmail.com>
2023-02-03 08:13:12 -0300

    correções e anotações-02

--- views/action_view_buscar_profissao.py
+++ views/action_view_buscar_profissao.py
@@ -22,24 +22,3 @@ class ActionViewBuscarProfissao:
             },
             "success": True
         }
-
-class ActionViewDelete:
-    def view_delete(self, request):
-        # ! nome = request.json
-        # ! user_agent = request.headers["User-Agent"]
-        body = request.json
-        delete = body["delete"]
-
-        action_controller_delete = ActionDelete()
-        # ! nome_validation = action_controller_delete.deletar_pessoa_por_nome(nome)
-        delete = action_controller_delete.deletar_pessoa_por_nome(delete)
-
-        return {
-            "status_code": 200,
-            # ! "data": {
-            # !     "validation": nome_validation,
-            # !     "userAgent": user_agent
-            # ! },
-            "data" : "",
-            "success": True
-        }
